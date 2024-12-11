9315264781ab6e05db3d5836e785db28c1157941
Kryszak <prajskrzysztof@gmail.com>
2024-12-11 14:27:25 +0100

    merge gradlew test invocations to single call due to not hitting rate limit

--- run-e2e-suite.sh
+++ run-e2e-suite.sh
@@ -2,41 +2,7 @@
 
 set -euo pipefail
 
-# Achievement endpoints
-./gradlew test --tests  io.github.kryszak.e2e.achievement.AchievementE2ETests
-
-# API info endpoints
-./gradlew test --tests io.github.kryszak.e2e.apiinfo.ApiInfoE2ETests
-
-# Commerce endpoints
-./gradlew test --tests io.github.kryszak.e2e.commerce.CommerceE2ETests
-
-# Daily rewards endpoints
-./gradlew test --tests io.github.kryszak.e2e.dailyrewards.DailyRewardsE2ETests
-
-# Game mechanics endpoints
-./gradlew test --tests io.github.kryszak.e2e.gamemechanics.GameMechanicsE2ETests
-
-# Guild public endpoints
-./gradlew test --tests io.github.kryszak.e2e.guild.GuildE2ETests
-
-# Home instance endpoints
-./gradlew test --tests io.github.kryszak.e2e.homeinstance.HomeInstanceE2ETests
-
-# Item endpoints
-./gradlew test --tests io.github.kryszak.e2e.items.ItemsE2ETests
-
-# Map info endpoints
-./gradlew test --tests io.github.kryszak.e2e.mapinfo.MapInfoE2ETests
-
-# Miscellaneous endpoints
-./gradlew test --tests io.github.kryszak.e2e.miscellaneous.MiscellaneousE2ETests
-
-# Miscellaneous endpoints
-./gradlew test --tests io.github.kryszak.e2e.pvp.PvPE2ETests
-
-# Story endpoints
-./gradlew test --tests io.github.kryszak.e2e.story.StoryE2ETests
-
-# WvW endpoints
-./gradlew test --tests io.github.kryszak.e2e.wvw.WvWE2ETests
+# In case of hitting rate limit errors split running tests
+# per test class and add 1 minute delay between runs
+# https://wiki.guildwars2.com/wiki/API:Best_practices#Rate_Limit
+./gradlew test
