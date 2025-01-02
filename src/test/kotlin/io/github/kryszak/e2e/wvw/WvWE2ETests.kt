42592c00997eb29658ead7ac7b9bec3e63092104
Kryszak <prajskrzysztof@gmail.com>
2025-01-02 15:39:06 +0100

    bump gwatlin version to 2.2.1, fixed wvw tests

--- src/test/kotlin/io/github/kryszak/e2e/wvw/WvWE2ETests.kt
+++ src/test/kotlin/io/github/kryszak/e2e/wvw/WvWE2ETests.kt
@@ -1,12 +1,11 @@
 package io.github.kryszak.e2e.wvw
 
+import io.github.kryszak.e2e.randomElements
 import io.github.kryszak.gwatlin.api.miscellaneous.GWMiscellaneousClient
 import io.github.kryszak.gwatlin.api.wvw.GWWvwClient
 import io.kotest.assertions.throwables.shouldNotThrowAny
-import io.kotest.assertions.throwables.shouldThrowAny
 import io.kotest.core.spec.style.ExpectSpec
 import io.kotest.core.test.TestCaseOrder
-import io.github.kryszak.e2e.randomElements
 
 class WvWE2ETests : ExpectSpec() {
     override fun testCaseOrder(): TestCaseOrder = TestCaseOrder.Sequential
@@ -24,10 +23,9 @@ class WvWE2ETests : ExpectSpec() {
                 val matchIds = client.getMatchIds()
                 shouldNotThrowAny { client.getMatches(matchIds) }
             }
-            // FIXME
             expect("Fetch objectives") {
                 val objectiveIds = client.getObjectiveIds().randomElements(100)
-                shouldThrowAny { client.getObjectives(objectiveIds) }
+                shouldNotThrowAny { client.getObjectives(objectiveIds) }
             }
             expect("Fetch ranks") {
                 val rankIds = client.getRankIds().randomElements(100)
